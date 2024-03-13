2a34a8da1f48988595fdfa206f75b3a6cfad2586
porygonisaduck <alvaradx@umich.edu>
2024-03-12 21:47:43 -0400

    Started working on add listing form

--- spaceshare/static/js/script.js
+++ spaceshare/static/js/script.js
@@ -10,7 +10,20 @@ function addListing() {
   
     var tempPage = document.createElement('div');
     tempPage.className = 'temp-page';
-    tempPage.innerHTML = '<h2>Temporary Page</h2><p>This is a temporary page.</p><button onclick="closeListing()">Close</button>';
+    tempPage.innerHTML = `
+      <i aria-label="exit-listing" class="fa-2x fa-solid fa-angle-left"></i>
+      <h3>Add Listing to Registered Place:</h3>
+      <i aria-label="map-pin" class="fa-2x fa-solid fa-map-pin"></i>
+      <form class="inline" action="" method="" enctype="">
+        <label for="textInput">Listing Headline</label>
+        <input type="text" name="headline" required/>
+        <label for="textInput">Description of Space</label>
+        <input type="text" name="description" required/>
+        <label for="textInput">Item Guidelines</label>
+        <input type="text" name="guidlines" required/>
+      </form>
+      
+      <button onclick="closeListing()">Close</button>`;
   
     // Append temporary page to overlay
     overlay.appendChild(tempPage);
